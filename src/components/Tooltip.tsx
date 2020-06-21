592cdcc49b5b2203b3a2dd3ddf55f5ce2db5f8aa
crystalboxes <ursus95@gmail.com>
2020-06-21 14:05:18 +0300

    Rewrite tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,104 +1,103 @@
-import React from 'react'
-import { View, TouchableHighlight, Text, LayoutChangeEvent, LayoutRectangle, StyleProp, ViewStyle } from 'react-native'
-
-type TooltipState = {
-  modalVisible: boolean,
-}
+import React, { useState, } from 'react'
+import { View, TouchableHighlight, LayoutChangeEvent, LayoutRectangle, StyleProp, ViewStyle, StyleSheet } from 'react-native'
 
 type TooltipProps = {
   content: any,
   backgroundColor?: string,
   borderWidth?: number,
   borderColor?: string,
-  style?: StyleProp<ViewStyle>
+  style?: StyleProp<ViewStyle>,
+  children: JSX.Element | JSX.Element[]
 }
 
 export enum TooltipPlacement {
   Top, Right, Bottom, Left
 }
 
-const tipSize = 6
 
-export class Tooltip extends React.Component<TooltipProps, TooltipState> {
-  contentLayout: LayoutRectangle | null = null
-  contentLayoutUpdated = false
+const useLayout = (): [LayoutRectangle | null, any] => {
+  const [layout, setLayout]: [LayoutRectangle | null, any] = useState(null)
 
-  childLayout: LayoutRectangle | null = null
-  childLayoutUpdated = false
-
-  state = { modalVisible: false }
-
-
-  onChildLayout(e: LayoutChangeEvent) {
-    if (!this.childLayoutUpdated) {
-      this.childLayout = e.nativeEvent.layout
-      this.childLayoutUpdated = true
-      this.forceUpdate()
-    }
-
-  }
-
-  onLayout(e: LayoutChangeEvent) {
-    if (!this.contentLayoutUpdated) {
-      this.contentLayout = e.nativeEvent.layout
-      this.contentLayoutUpdated = true
-      this.forceUpdate()
+  const onLayout = (e: LayoutChangeEvent) => {
+    if (layout == null) {
+      setLayout(e.nativeEvent.layout)
     }
   }
 
-  render() {
-    const tooltipStyle = {
-      opacity: this.childLayoutUpdated && this.contentLayoutUpdated ? 1.0 : 0.0,
-      top: (() => {
-        if (this.childLayout != null) {
-          return this.childLayout.height / 2
-        }
-        return 0
-      })(),
-      left: 30
-    }
-    const tooltipContentStyle = { marginTop: this.contentLayout != null ? this.contentLayout.height / 2 * -1 : 0 }
-
-    return (
-      <View>
-        {this.state.modalVisible
-          ? (<View style={{
-            ...tooltipStyle,
-            position: 'absolute',
-            flex: 1,
-            flexDirection: "row",
-            zIndex: 200,
-          }}>
-            <View style={{
-              width: tipSize,
-              height: tipSize,
-              backgroundColor: this.props.backgroundColor ? this.props.backgroundColor as string : 'white',
-              borderLeftWidth: this.props.borderWidth || 1,
-              borderLeftColor: this.props.borderColor || 'black',
-              borderBottomWidth: this.props.borderWidth || 1,
-              borderBottomColor: this.props.borderColor || 'black',
-              transform: [{ rotate: '45deg' }],
-              zIndex: 300,
-            }}></View>
-            <View onLayout={e => this.onLayout(e)} style={[{
-              ...tooltipContentStyle,
-              marginLeft: -tipSize / 2,
-              paddingLeft: tipSize / 2,
-              backgroundColor: this.props.backgroundColor ? this.props.backgroundColor as string : 'white',
-              borderWidth: this.props.borderWidth || 1,
-              borderColor: this.props.borderColor || 'black',
-            }, this.props.style]}>{this.props.content}</View></View>)
-          : null
-        }
-        <TouchableHighlight
-          onPress={() => {
-            this.setState({ modalVisible: !this.state.modalVisible })
-          }}>
-          <View onLayout={e => this.onChildLayout(e)}>
-            {this.props.children}
-          </View>
-        </TouchableHighlight>
-      </View>
-    )
-  }
+  return [layout, onLayout]
 }
+
+export const Tooltip = ({ backgroundColor, borderWidth, borderColor,
+  style,
+  content,
+  children }: TooltipProps) => {
+  const [visible, setVisible] = useState(false)
+  const [contentLayout, onContentLayout] = useLayout()
+  const [childLayout, onChildLayout] = useLayout()
+
+  const { containerStyle, tooltipContentStyle, arrowStyle } = (() => {
+    return {
+      containerStyle: {
+        opacity: childLayout && contentLayout ? 1.0 : 0.0,
+        top: (() => {
+          if (childLayout != null) {
+            return childLayout.height / 2
+          }
+          return 0
+        })(),
+        left: 30
+      },
+      tooltipContentStyle: {
+        marginTop: contentLayout != null
+          ? contentLayout.height / 2 * -1
+          : 0,
+        backgroundColor: backgroundColor ? backgroundColor as string : 'white',
+        borderWidth: borderWidth || 1,
+        borderColor: borderColor || 'black',
+      },
+      arrowStyle: {
+        backgroundColor: backgroundColor ? backgroundColor as string : 'white',
+        borderLeftWidth: borderWidth || 1,
+        borderLeftColor: borderColor || 'black',
+        borderBottomWidth: borderWidth || 1,
+        borderBottomColor: borderColor || 'black',
+      }
+    }
+  })()
+
+  return (
+    <View>
+      {visible
+        ? (<View style={[containerStyle, styles.container]}>
+          <View style={[arrowStyle, styles.arrow]}></View>
+          <View onLayout={onContentLayout} style={[tooltipContentStyle, styles.content, style]}>{content}</View></View>)
+        : null
+      }
+      <TouchableHighlight onPress={() => { setVisible(!visible) }}>
+        <View onLayout={onChildLayout}>
+          {children}
+        </View>
+      </TouchableHighlight>
+    </View >
+  )
+}
+
+const tipSize = 6
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    flex: 1,
+    flexDirection: "row",
+    zIndex: 200,
+  },
+  content: {
+    marginLeft: -tipSize / 2,
+    paddingLeft: tipSize / 2,
+  },
+  arrow: {
+    width: tipSize,
+    height: tipSize,
+    transform: [{ rotate: '45deg' }],
+    zIndex: 300,
+  }
+})
